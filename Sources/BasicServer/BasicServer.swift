d9ccb7d19371b2ca14a2903eaa0665e9d8fa66a4
Matthew Kupinski <kupinski@mac.com>
2022-03-28 11:04:54 -0700

    Initial version.  Will it work?

--- Sources/BasicServer/BasicServer.swift
+++ Sources/BasicServer/BasicServer.swift
@@ -1,6 +1,71 @@
-public struct BasicServer {
-    public private(set) var text = "Hello, World!"
+import Network
+import Foundation
 
-    public init() {
+
+
+/// A TCP server that will stream H-Matrix data (rows or columns) to the client
+public class SystemTransformServer<T: Connection> {
+    /// The port number to connect to
+    public var port: Int
+    
+    private var connectionType: T.Type
+    
+    /// A description of the server status.  Check `listener.status` for actual status.
+    public var serverStatus = ""
+    
+    // Network server and connection variables
+    /// The listener waiting for client connections
+    private var listener: NWListener
+        
+    /// Is the network busy transferring data.  If so, this will halt a request for more data.
+    public var networkBusy = false
+        
+    
+    /// A ListModeServer is used to model the camera server and requires a specific camera model to attach to as well as a ``dataProvider`` that will provide the data to send when requested by the client.
+    /// - Parameters:
+    ///   - forCamera: The camera model to attach to
+    ///   - dataProvider: How the data are provided to this server.
+    public init(withConnectionManager: T.Type, onPort: Int) {
+        port = onPort
+        connectionType = withConnectionManager
+        
+        listener = try! NWListener(using: .tcp, on: NWEndpoint.Port(integerLiteral: UInt16(port)))
+
+        listener.stateUpdateHandler = serverStateUpdate(to: )
+        listener.newConnectionHandler = newConnection(to: )
+        
+        listener.start(queue: .main)
     }
+    
+    
+    /// A new commention has been requested.  Spawn a new process to serve this client.  NOTE.  Only one client is allowed to connect to the camera.
+    /// - Parameter connection: The connection object
+    public func newConnection(to connection: NWConnection) {
+
+        let _ = T(connection)
+    }
+    
+    
+    
+    /// The status of the server.
+    /// - Parameter state: Changed server state
+    func serverStateUpdate(to state: NWListener.State) {
+        switch (state) {
+        case .setup:
+            self.serverStatus = "Setting up Server"
+        case .waiting(let error):
+            self.serverStatus = "Waiting with status: \(error)"
+        case .ready:
+            self.serverStatus = "Ready for connections"
+        case .failed(let error):
+            self.serverStatus = "Failed with status: \(error)"
+        case .cancelled:
+            self.serverStatus = "Server cancelled"
+        default:
+            self.serverStatus = "Unknown state = \(state)"
+        }
+        print(self.serverStatus)
+    }
+
+    
 }
---
+++ Sources/BasicServer/Connection.swift
@@ -0,0 +1,168 @@
+//
+//  File.swift
+//  
+//
+//  Created by Matthew Kupinski on 3/28/22.
+//
+
+import Foundation
+import Network
+
+
+
+public enum ConnectionError : Error {
+    case invalidArguments(args: [String])
+}
+
+
+public protocol Connection {
+    var connection: NWConnection { get set }
+    
+    /// A description of the client connection status.  Check `connection.status` for actual status.
+    var connectionStatus: String { get set }
+    
+    /// Specify the command strings,  their number of arguments, and the command method to call
+    var commands: [(string: String, numArgs: Int, command: ([String])throws->()  )] { get set }
+    
+    /// Create a new server connection
+    /// - Parameter connection: The Network.framework connection
+    init(_ connection: NWConnection)
+    
+    /// Parse a network message.
+    /// - Parameter message: The message with white space or new lines separating out the various arguments.
+    func parseNetworkData(_ message: String)
+    
+    /// Listen for commands and parse them.
+    func awaitCommands()
+    
+    /// Change the status of the client connection
+    /// - Parameter newState: The new state
+   func connectionStateUpdate(to newState: NWConnection.State)
+    
+    
+    /// The connection failed.  Stop everything
+    /// - Parameter error: The error message
+   func connectionDidFail(error: Error)
+    
+    /// The connection ended.  Stop everything
+    func connectionDidEnd()
+    
+    /// When a connection stops, we need to cancel it and set the connection variable to nil.  This will allow the client to reconnect if needed.
+    /// - Parameter error: The error
+    func stop(error: Error?)
+}
+
+public extension Connection {
+
+    
+    init(_ connection: NWConnection) {
+        self.init(connection)
+        
+        self.connection = connection
+        
+        let connectionQueue = DispatchQueue(label: "New Connection",
+                                            qos: .userInitiated,
+                                            attributes: [],
+                                            autoreleaseFrequency: .inherit,
+                                            target: nil)
+        
+        connection.stateUpdateHandler = connectionStateUpdate(to: )
+        
+        awaitCommands()
+        
+        connection.start(queue: connectionQueue)
+    }
+        
+    
+    func parseNetworkData(_ message: String) {
+        var stringArray = message.components(separatedBy: .whitespacesAndNewlines)
+        
+        // First string is the command
+        let enteredCommand = stringArray.remove(at: 0)
+        
+        // All others are arguments.  Filter out the empty ones.
+        let arguments = stringArray.filter({$0 != ""})
+        let numArgs = arguments.count
+        
+        let matchingCommand = self.commands.filter({
+            ($0.string == enteredCommand) && ($0.numArgs == numArgs)
+        })
+        if matchingCommand.count == 1 {
+            do {
+                try matchingCommand[0].command(arguments)
+            }
+            catch ConnectionError.invalidArguments(args: arguments) {
+                print("Warning: The command \"\(enteredCommand)\" was passed the following invalid arguments \(arguments)")
+            }
+            catch {
+                print("Warning: Unknown error \(error)")
+            }
+        } else {
+            print("Warning: Unknown command \"\(enteredCommand)\" with \(numArgs) arguments: \(arguments)")
+        }
+    }
+    
+    
+    func awaitCommands() {
+        
+        connection.receive(minimumIncompleteLength: 1, maximumLength: 512)  { (data, _, isComplete, error) in
+            if let data = data, !data.isEmpty {
+                while (!data.isEmpty) {
+                    guard let message = String(data: data, encoding: .utf8) else {
+                        fatalError("Data Sync Error.  Awaiting command.")
+                    }
+                    
+                    self.parseNetworkData(message)
+                }
+            }
+            if isComplete {
+                self.connectionDidEnd()
+            } else if let error = error {
+                self.connectionDidFail(error: error)
+            } else {
+                self.awaitCommands()
+            }
+        }
+    }
+    
+    mutating func connectionStateUpdate(to newState: NWConnection.State) {
+        switch (newState) {
+        case .ready:
+            connectionStatus = "Ready"
+        case .setup:
+            connectionStatus = "Setup"
+        case .cancelled:
+            connectionStatus = "Cancelled"
+        case .preparing:
+            connectionStatus = "Preparring"
+        case .waiting(let inErr):
+            connectionStatus = "Waiting \(inErr)"
+            connectionDidFail(error: inErr)
+        case .failed(let inErr):
+            connectionStatus = "Failed \(inErr)"
+            connectionDidFail(error: inErr)
+        default:
+            break
+        }
+    }
+    
+    func connectionDidFail(error: Error) {
+        print("Connection did fail, error: \(error)")
+        stop(error: error)
+    }
+    
+    private func connectionDidEnd() {
+        print("Connection ended without error")
+        self.stop(error: nil)
+    }
+    
+    /// When a connection stops, we need to cancel it and set the connection variable to nil.  This will allow the client to reconnect if needed.
+    /// - Parameter error: The error
+    private func stop(error: Error?) {
+        self.connection.stateUpdateHandler = nil
+        self.connection.cancel()
+    }
+    
+}
+
+
