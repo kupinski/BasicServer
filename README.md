75b081a2deca8b3bcc10507c288586867819bbce
Matthew Kupinski <kupinski@mac.com>
2022-03-28 09:47:17 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BasicServer
+
+A description of this package.
---
+++ Tests/BasicServerTests/BasicServerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import BasicServer
+
+final class BasicServerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(BasicServer().text, "Hello, World!")
+    }
+}
